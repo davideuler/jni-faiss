5dac1bd47aed082f8cb5e1306752914ba5988b5b
david ullua <davidullua@ihopeit.com>
2022-09-28 14:07:27 +0800

    auto detect OS and load different dynamic library

--- common/src/main/java/com/gameofdimension/faiss/utils/JniFaissInitializer.java
+++ common/src/main/java/com/gameofdimension/faiss/utils/JniFaissInitializer.java
@@ -14,9 +14,22 @@ public class JniFaissInitializer {
 
   static {
     try {
-      NativeUtils.loadLibraryFromJar("/_swigfaiss.so");
+      String os = System.getProperty("os.name"); // Linux, Mac OS X
+
+      String osArch = System.getProperty("os.arch"); // aarch64 for mac m1
+
+      if("Linux".equalsIgnoreCase(os)){
+        NativeUtils.loadLibraryFromJar("/_swigfaiss.so");
+      } else if("aarch64".equalsIgnoreCase(osArch)){ //mac m1
+        NativeUtils.loadLibraryFromJar("/_swigfaiss_mac_arm64.so");
+      } else { // mac intel
+        NativeUtils.loadLibraryFromJar("/_swigfaiss_mac.so");
+      }
+      
       initialized = true;
     } catch (IOException e) {
+      System.err.println("Error loading _swigfaiss library");
+      e.printStackTrace();
       Preconditions.checkArgument(false);
     }
   }
